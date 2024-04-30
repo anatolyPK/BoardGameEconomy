853763dc0dd79c1a16d9c2fe3ddda101fea55754
Anatoly <89687109+anatolyPK@users.noreply.github.com>
2024-04-30 17:15:33 +1000

    основной функционал готов

--- src/config/project_config.py
+++ src/config/project_config.py
@@ -1,19 +1,26 @@
-import dotenv
+import os
+
 from dotenv import load_dotenv
 
 from pydantic_settings import BaseSettings
 
 
 load_dotenv()
-print('ПЕРЕМЕННЫЕ!!!!')
-print(dotenv.dotenv_values())
+
+
+db_echo = os.getenv('DB_ECHO')
+project_name = os.getenv('PROJECT_NAME')
+version = os.getenv('VERSION')
+debug = os.getenv('DEBUG')
+cors = os.getenv('CORS_ALLOWED_ORIGINS')
+
 
 class Settings(BaseSettings):
-    DB_ECHO: bool
-    PROJECT_NAME: str
-    VERSION: str
-    DEBUG: bool
-    CORS_ALLOWED_ORIGINS: str
+    DB_ECHO: bool = db_echo
+    PROJECT_NAME: str = project_name
+    VERSION: str = version
+    DEBUG: bool = debug
+    CORS_ALLOWED_ORIGINS: str = cors
 
 
 settings = Settings()
